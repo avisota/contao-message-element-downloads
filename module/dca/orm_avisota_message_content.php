e752e18d0e410130e635062ad71200918b430b8e
Tristan Lins <tristan.lins@bit3.de>
2014-02-21 05:00:23 +0100

    More refactoring and splitting.

---
+++ module/dca/orm_avisota_message_content.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-message-element-hyperlink
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Table orm_avisota_message_content
+ * Entity Avisota\Contao:MessageContent
+ */
---
+++ module/languages/en/default.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-core
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Mailing content elements
+ */
+$GLOBALS['TL_LANG']['MCE']['downloads'] = array('Downloads', 'Generates multiple links to download files.');
