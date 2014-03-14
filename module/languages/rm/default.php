c3b5b2af105cfa18f5e83df147f0e773dfb9731a
Avibot <avibot@avisota.org>
2014-03-14 04:00:35 +0100

    Auto-Update translations from transifex

---
+++ module/languages/rm/default.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-03-11T16:02:02+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['MCE']['downloads']['0'] = 'Telechargiadas';
+$GLOBALS['TL_LANG']['MCE']['downloads']['1'] = 'Generescha colliaziuns per telechargiar datotecas.';
+
